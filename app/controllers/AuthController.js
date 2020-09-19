23716a6a730178c95c81404476fe0fef6522d868
Agam Adhityo <adhityoagam@gmail.com>
2020-09-20 05:29:22 +0700

    :airplane: update validator to use indicative

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,60 +1,69 @@
 const { User } = require('../models/Models');
 const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const JoiErrorFormatter = require('../helpers/JoiErrorFormatter');
+const { validations, validateAll } = require('indicative/validator')
+const { sanitizations, sanitize } = require('indicative/sanitizer')
+const IndicativeErrorFormatter = require('../helpers/IndicativeErrorFormatter');
 const day = require('dayjs');
 const bcrypt = require('bcrypt')
 
 const AuthController = {
 
   login: async (req, res) => {
-    const schema = Joi.object({
-      email: Joi.required(),
-      password: Joi.required()
-    });
-
-    try {
-      const validate = await schema.validateAsync(req.body);
-    } catch (error) {
-      return res.status(422).json({
-        message: 'The given data was invalid',
-        errors: JoiErrorFormatter.formats(error.details)
-      })
-    }
-
-    const isUserExist = await User.findOne({ 
-      where: {
-        email: req.body.email,
-      }
-    });
-
-    if (!isUserExist) {
-      return res.status(401).json({
-        'message': 'Username or password is incorrect'
-      })
-    }
-
-    if (!bcrypt.compareSync(req.body.password, isUserExist.password)) {
-      return res.status(401).json({
-        'message': 'Username or password is incorrect'
-      })
-    }
-
-    const data = {
-      id: isUserExist.id,
-      email: isUserExist.email,
-      name: isUserExist.name
+    
+    const rules = {
+      'email': [validations.required(), validations.email()],
+      'password': [validations.required()]
     };
 
-    const token = jwt.sign({
-      'iat': day().unix(),
-      'exp': day().add(10, 'day').unix(),
-    }, process.env.JWT_SECRET);
+    const sanitizer = {
+      'email': [sanitizations.normalizeEmail()],
+      'password': [sanitizations.trim(), sanitizations.escape()],
+    }
 
-    return res.json({
-      'access_token': token,
-      data
-    })
+    sanitize(req.body, sanitizer);
+    
+    validateAll(req.body, rules, IndicativeErrorFormatter.messages())
+      .then( async (data) => {
+        const isUserExist = await User.findOne({ 
+          where: {
+            email: req.body.email,
+          }
+        });
+    
+        if (!isUserExist) {
+          return res.status(401).json({
+            'message': 'Username or password is incorrect'
+          })
+        }
+    
+        if (!bcrypt.compareSync(req.body.password, isUserExist.password)) {
+          return res.status(401).json({
+            'message': 'Username or password is incorrect'
+          })
+        }
+    
+        const userData = {
+          id: isUserExist.id,
+          email: isUserExist.email,
+          name: isUserExist.name
+        };
+    
+        const token = jwt.sign({
+          'iat': day().unix(),
+          'exp': day().add(10, 'day').unix(),
+        }, process.env.JWT_SECRET);
+    
+        return res.json({
+          'access_token': token,
+          userData
+        })
+      })
+      .catch( (err) => {
+        return res.status(422).json({
+          message: 'The given data was invalid',
+          errors: IndicativeErrorFormatter.format(err)
+        })
+      });
   }
 }
 
