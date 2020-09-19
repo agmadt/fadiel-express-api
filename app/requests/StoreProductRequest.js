11b935a548f0d34938db250913ac84cf4f0a90b6
Agam Adhityo <adhityoagam@gmail.com>
2020-09-20 04:57:41 +0700

    :airplane: add product request

---
+++ app/requests/StoreProductRequest.js
@@ -0,0 +1,26 @@
+const { validations } = require('indicative/validator')
+const { sanitizations } = require('indicative/sanitizer')
+
+const rules = {
+  'name': [ validations.required() ],
+  'price': [ validations.required() ],
+  'images': [ validations.array() ],
+  'images.*': [ validations.object() ],
+  'images.*.image': [ validations.required() ],
+  'variants': [ validations.array() ],
+  'variants.*': [ validations.object() ],
+  'variants.*.name': [ validations.required() ],
+  'variants.*.options': [ validations.array(), validations.required(), validations.min([1]) ],
+  'variants.*.options.*': [ validations.object() ],
+  'variants.*.options.*.name': [ validations.required() ],
+}
+
+const sanitizer = {
+  'name': [sanitizations.stripTags()],
+  'price': [sanitizations.stripTags()],
+  'images.*.image': [sanitizations.stripTags()],
+  'variants.*.name': [sanitizations.stripTags()],
+  'variants.*.options.*.name': [sanitizations.stripTags()],
+}
+
+module.exports = { rules, sanitizer }
