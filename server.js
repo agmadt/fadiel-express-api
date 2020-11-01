ccc30be54d07864bd34a2f2c8bb4610b9896cbc5
Agam Adhityo <adhityoagam@gmail.com>
2020-11-02 05:35:59 +0700

    :airplane: Add CORS

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
